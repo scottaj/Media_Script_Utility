df964a451792565c124a9bf5609ba63e23b589c8
scottaj <aljscott1990@gmail.com>
2010-07-31 17:44:24 -0600

    Added event code to update time display widgets

--- lib/cue_gui_event.rb
+++ lib/cue_gui_event.rb
@@ -6,8 +6,14 @@ require 'media_player'
 
 module GUI
 
+    # = CueGUIEvent
+    #
+    # This class contains all event handling code for the main GUI window.
+    # It also initializes and manages the media player.
     class CueGUIEvent < CueGUI
 
+        # Creates the main GUI window initalizing all event handlers.
+        # Also creates a MediaPlayer and a timer to moniter playback time.
         def initialize
             super
 
@@ -19,15 +25,38 @@ module GUI
                     if @media_player.media_state == 1
                         set_media_time(@media_player.tell)
                     end
+                else
+                    @total_time.set_label("0:00")
                 end
             end
-            media_t.start(50)
-
-
+            media_t.start(75)
+            @def_c = @subsec_btn_0.get_background_colour()
+            
+            
         end
 
+        # Changes all the widgets relating to displaying the current time.
         def set_media_time(raw_time)
-            
+            tenth = (raw_time / 10).to_i
+            sec = (raw_time / 1000) % 60
+            min = (raw_time / 1000 / 60).to_i
+            hour = (min / 60).to_i
+
+            # Set toggle button color.
+            buttons = [@subsec_btn_0, @subsec_btn_1, @subsec_btn_2,
+                @subsec_btn_3, @subsec_btn_4, @subsec_btn_5, @subsec_btn_6,
+                @subsec_btn_7, @subsec_btn_8, @subsec_btn_9
+            ]
+
+            buttons.each {|b| b.set_background_colour(@def_c)}
+            button = buttons[tenth]
+            button.set_background_colour(RED)
+
+            # Set static text value
+            @total_time.set_label("#{"#{hour}:" if hour >= 1}#{min}:#{0 if sec < 10}#{sec}")
+
+            # Move Slider
+            @song_time_slider.set_value(sec)
         end
     end
 end
