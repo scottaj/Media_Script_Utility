17c23b7215bcf360a5e7ac80bb21b8b4f12fb909
scottaj <aljscott1990@gmail.com>
2010-08-02 21:42:58 -0600

    Added methods to CueScript class

--- lib/cue_script.rb
+++ lib/cue_script.rb
@@ -1,9 +1,29 @@
 # Copyright(c) 2010 Al Scott
 # License details can be found in the LICENSE file.
 
+require 'cue'
+
 class CueScript
   
-    def initialize()
-    
+    def initialize
+        @script = {}
+    end
+
+    def add_cue(time, length = 100, value = 0, comment = "")
+        key = "#{time[0]}:#{time[1]}:#{time[2]}:#{time[3]}"
+
+        @script[key] = Cue.new(length, value, comment)
+    end
+
+    def remove_cue(time)
+        key = "#{time[0]}:#{time[1]}:#{time[2]}:#{time[3]}"
+
+        @script.delete(key)
+    end
+
+    def cue_finder(time)
+        key = "#{time[0]}:#{time[1]}:#{time[2]}:#{time[3]}"
+
+        return @script[key]
     end
 end
